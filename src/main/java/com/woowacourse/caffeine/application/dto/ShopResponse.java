5688c6081b0521408f909c541c70eae78b5461d6
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-12-09 18:08:06 +0900

    refactor: dto.ShopResponse 수정

--- src/main/java/com/woowacourse/caffeine/application/dto/ShopResponse.java
+++ src/main/java/com/woowacourse/caffeine/application/dto/ShopResponse.java
@@ -1,9 +1,13 @@
 package com.woowacourse.caffeine.application.dto;
 
 public class ShopResponse {
-
     public long id;
     public String name;
+
+    // image url
+    private String image;
+    private String address;
+    private String phoneNumber;
 
     public ShopResponse() {
     }
@@ -11,6 +15,14 @@ public class ShopResponse {
     public ShopResponse(final long id, final String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public ShopResponse(long id, String name, String image, String address, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
     }
 
     public long getId() {
@@ -27,5 +39,29 @@ public class ShopResponse {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
